7744a14d6f900d503e3e05caf86037389d9cbc98
unknown <s.m.d4321@gmail.com>
2022-09-17 20:02:43 +0300

    single page app 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Params ,ActivatedRoute} from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from "../shared/dish";
+import { DishService } from '../services/dish.service';
+import { ArrowViewStateTransition } from '@angular/material';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-  @Input()
-  dish:Dish;
-  constructor() { }
+
+  dish: Dish;
+  constructor( private dishServcie:DishService ,
+    private route:ActivatedRoute,
+    private location:Location) { }
 
   ngOnInit() {
+    let id =this.route.snapshot.params['id'];
+    this.dish = this.dishServcie.getDish(id);
   }
-
+  goBack(): void {
+    this.location.back();
+  }
 }
