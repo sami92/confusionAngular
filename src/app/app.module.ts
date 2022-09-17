21f06e391098cb0cfe2f118c6de8562c41ca34fb
unknown <s.m.d4321@gmail.com>
2022-09-17 19:33:40 +0300

    single page app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,18 @@ import { AppComponent } from "./app.component";
 import "hammerjs";
 import { MenuComponent } from "./menu/menu.component";
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component'
+
+import {DishService} from './services/dish.service';
+import {PromotionService} from './services/promotion.service';
+ 
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailComponent],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent, HomeComponent, AboutComponent, ContactComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -23,8 +33,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule,
     FlexLayoutModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService,PromotionService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Dish } from "../shared/dish";
-import { DISHES } from "../shared/dishes";
+import { DishService } from "../services/dish.service";
+// import { DISHES } from "../shared/dishes";
 @Component({
   selector: "app-menu",
   templateUrl: "./menu.component.html",
@@ -9,13 +10,15 @@ import { DISHES } from "../shared/dishes";
 
 
 export class MenuComponent implements OnInit {
-  dishes = DISHES;
+  dishes : Dish[];
 
   selectedDish : Dish;
 
-  constructor() {}
+  constructor (private dishService:DishService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.dishes = this.dishService.getDishes()
+  }
 
   onSelect(dish:Dish){
     this.selectedDish=dish
