9ad81bc50dad3ab3f3dc36cad7f49eca1fcbc3fc
unknown <s.m.d4321@gmail.com>
2022-10-17 22:31:41 +0300

    RxJS Part 2

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
-import {Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
@@ -10,10 +10,10 @@ export class PromotionService {
 
   constructor() { }
   getPromotions(): Observable<Promotion[]> {
-    return of(PROMOTIONS).pipe(delay(2000));    
+    return of(PROMOTIONS).pipe(delay(2000));
   }
   getPromotion(id: string): Observable<Promotion> {
-    return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));   
+    return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
   }
   getFeaturedPromotion(): Observable<Promotion> {
     return of(PROMOTIONS.filter((dish) => dish.featured)[0]).pipe(delay(2000));
